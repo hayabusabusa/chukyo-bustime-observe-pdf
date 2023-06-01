b3e83e182c84ee005f088acb2fa6d7932a772834
hayabusabusa <hayabusabusaSY@gmail.com>
2023-06-02 02:55:33 +0900

    test: ファイル操作関連のテストを追加

---
+++ src/repository/LocalStorageRepository.test.ts
@@ -0,0 +1,38 @@
+import * as fs from "fs";
+
+import { LocalStorageRepository } from "./index";
+
+describe("基本的なファイル操作の確認", () => {
+  test("ファイルの保存ができること", async () => {
+    const path = "test.txt"
+    const repository = new LocalStorageRepository();
+  
+    const buffer = Buffer.from("test");
+    await repository.save(path, buffer);
+  
+    expect(fs.existsSync(path)).toBe(true);
+  });
+
+  test("ファイルの読み込みができること", async () => {
+    const path = "test.txt"
+    const text = "test"
+    const repository = new LocalStorageRepository();
+
+    const buffer = Buffer.from(text);
+    await repository.save(path, buffer);
+
+    const file = await repository.load(path);
+    expect(file.toString("utf-8")).toBe(text);
+  });
+
+  test("ファイルの削除ができること", async () => {
+    const path = "test.txt"
+    const repository = new LocalStorageRepository();
+
+    const buffer = Buffer.from("test");
+    await repository.save(path, buffer);
+    await repository.unlinkAll([path]);
+
+    expect(fs.existsSync(path)).toBe(false);
+  });
+});
