2bb403c332d71908695166a79f06d5215fa778fd
hayabusabusa <hayabusabusaSY@gmail.com>
2023-06-02 20:18:36 +0900

    feat: 定数の定義を追加

---
+++ src/types/Const.ts
@@ -0,0 +1,15 @@
+export const URLs = {
+  pdf: "https://www.chukyo-u.ac.jp/support/pdf/studentlife/buscallender2023.pdf",
+  timeTable: "https://www.chukyo-u.ac.jp/support/pdf/studentlife/bustime.pdf",
+};
+
+export const PDFSize = {
+  pdf: {
+    width: 594,
+    height: 841
+  },
+  timeTable: {
+    width: 728,
+    height: 1031
+  }
+}
