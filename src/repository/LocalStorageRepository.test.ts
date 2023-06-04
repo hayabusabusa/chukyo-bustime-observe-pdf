b4af996feb2e5be4e9af56ed896b97648772eda9
hayabusabusa <hayabusabusaSY@gmail.com>
2023-06-04 20:27:49 +0900

    test: ローカルファイルを上書き保存するテストを追加

--- src/repository/LocalStorageRepository.test.ts
+++ src/repository/LocalStorageRepository.test.ts
@@ -3,10 +3,16 @@ import * as fs from "fs";
 import { LocalStorageRepository } from "./index";
 
 describe("基本的なファイル操作の確認", () => {
+  const path = "test.txt"
+  const repository = new LocalStorageRepository();
+
+  afterEach(async () => {
+    if (repository.isExists(path)) {
+      await repository.unlinkAll([path])
+    }
+  });
+
   test("ファイルの保存ができること", async () => {
-    const path = "test.txt"
-    const repository = new LocalStorageRepository();
-  
     const buffer = Buffer.from("test");
     await repository.save(path, buffer);
   
@@ -14,10 +20,7 @@ describe("基本的なファイル操作の確認", () => {
   });
 
   test("ファイルの読み込みができること", async () => {
-    const path = "test.txt"
     const text = "test"
-    const repository = new LocalStorageRepository();
-
     const buffer = Buffer.from(text);
     await repository.save(path, buffer);
 
@@ -25,10 +28,18 @@ describe("基本的なファイル操作の確認", () => {
     expect(file.toString("utf-8")).toBe(text);
   });
 
-  test("ファイルの削除ができること", async () => {
-    const path = "test.txt"
-    const repository = new LocalStorageRepository();
+  test("すでに存在するファイルに上書き保存ができること", async () => {
+    const buffer = Buffer.from("test");
+    await repository.save(path, buffer);
 
+    const buffer2 = Buffer.from("test2");
+    await repository.save(path, buffer2)
+  
+    const stored = await repository.load(path);
+    expect(stored.toString("utf-8")).toBe("test2");
+  });
+
+  test("ファイルの削除ができること", async () => {
     const buffer = Buffer.from("test");
     await repository.save(path, buffer);
     await repository.unlinkAll([path]);
