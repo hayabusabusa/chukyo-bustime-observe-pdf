a3d552d4c0b1fdaa0e0d8138415c8a428ce1d4fb
hayabusabusa <hayabusabusaSY@gmail.com>
2023-06-04 13:50:30 +0900

    fix: 確認用の実装を削除

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,6 @@ import { LocalStorageRepository } from "./repository";
 
 async function main() {
   dotenv.config();
-
-  const repository = new LocalStorageRepository();
-  const buffer = Buffer.from("sample");
-  await repository.save("sample.txt", buffer);
 }
 
 main();
