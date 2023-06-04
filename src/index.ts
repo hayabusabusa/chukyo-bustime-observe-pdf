e4e01e72200ed21067c110263ddbfda7bfcb57cf
hayabusabusa <hayabusabusaSY@gmail.com>
2023-06-04 15:11:04 +0900

    feat: スクリプト本体の処理を実装

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
 import * as dotenv from "dotenv";
 
-import { LocalStorageRepository } from "./repository";
+import { ObservePDFUseCase } from "./usecase";
+import { 
+  LocalStorageRepository, 
+  NetworkRepository, 
+  PixelmatchRepository 
+} from "./repository";
 
 async function main() {
   dotenv.config();
+
+  const useCase = new ObservePDFUseCase(
+    new LocalStorageRepository(),
+    new NetworkRepository(),
+    new PixelmatchRepository(),
+  );
+
+  await useCase.execute();
 }
 
 main();
