7d002088015bb94eae2e55a09a7e41716899de1d
hayabusabusa <hayabusabusaSY@gmail.com>
2024-08-15 22:52:45 +0900

    fix: UseCase で画像比較の際に利用するサイズの指定を修正
    固定でカレンダーの画像のサイズを利用してしまっていたため、外からサイズを指定するように修正。

--- src/usecase/ObservePDFUseCase.ts
+++ src/usecase/ObservePDFUseCase.ts
@@ -15,9 +15,23 @@ export class ObservePDFUseCase {
 
   async execute(): Promise<void> {
     // 運行カレンダーの PDF の差分を確認.
-    const compareCalendarPDFPromise = this.comparePDF(URLs.pdf, "calendar");
+    const compareCalendarPDFPromise = this.comparePDF(
+      URLs.pdf,
+      "calendar",
+      {
+        width: PNGSize.calendar.width,
+        height: PNGSize.calendar.height
+      }
+    );
     // 時刻表の PDF の差分を確認.
-    const compareTimetablePDFPromise = this.comparePDF(URLs.timeTable, "timetable");
+    const compareTimetablePDFPromise = this.comparePDF(
+      URLs.timeTable, 
+      "timetable",
+      {
+        width: PNGSize.timeTable.width,
+        height: PNGSize.timeTable.height
+      }
+    );
 
     await Promise.all([
       compareCalendarPDFPromise, 
@@ -31,7 +45,14 @@ export class ObservePDFUseCase {
    * @param fileName PNG として保存する際のファイル名.
    * @returns 非同期処理の完了.
    */
-  private async comparePDF(url: string, fileName: string): Promise<void> {
+  private async comparePDF(
+    url: string, 
+    fileName: string, 
+    size: {
+      width: number,
+      height: number
+    }
+  ): Promise<void> {
     // 比較するため PDF を PNG に変換.
     const originalPDF = await this.networkRepository.fetchPDF(url);
     const originalPNG = await this.networkRepository.convertPDFToPNG(originalPDF);
@@ -45,7 +66,14 @@ export class ObservePDFUseCase {
     }
 
     const cachedOriginalPNG = await this.localStorageRepository.load(cachedOriginalPNGPath);
-    const pixelmatchResponse = this.pixelmatchRepository.match(cachedOriginalPNG, originalPNG, { width: PNGSize.calendar.width, height: PNGSize.calendar.height });
+    const pixelmatchResponse = this.pixelmatchRepository.match(
+      cachedOriginalPNG,
+      originalPNG,
+      { 
+        width: size.width, 
+        height: size.height
+      }
+    );
 
     // 画像が一致していて差分がない場合はそのまま終了.
     if (pixelmatchResponse.isMatched) {
